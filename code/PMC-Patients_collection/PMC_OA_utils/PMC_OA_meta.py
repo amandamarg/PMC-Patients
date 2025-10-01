231d4bf0687d726d8d6f1dd74b4f7b25f7fdee4f
amandamarg <ammargo@umich.edu>
2025-10-01 13:42:54 -0400

    extract data before get cite

--- code/PMC-Patients_collection/PMC_OA_utils/PMC_OA_meta.py
+++ code/PMC-Patients_collection/PMC_OA_utils/PMC_OA_meta.py
@@ -24,34 +24,42 @@ file_paths = []
 PMIDs = []
 PMID_set = set()
 Licenses = []
+tar_gz_paths = []
 for subset in subsets:
-    directory = os.path.join(data_dir, subset, 'txt')
+    directory = os.path.join(data_dir, subset, 'xml')
     csvs = filter(lambda x: x.endswith(".csv"), os.listdir(directory))
     
     for csv in tqdm(list(csvs)):
-        filelist = pd.read_csv(os.path.join(directory, csv))
-        for i in range(len(filelist)):
-            file_path = filelist['Article File'].iloc[i]
-            PMID = str(filelist['PMID'].iloc[i])
-            License = filelist['License'].iloc[i]
-            if not PMID:
-                PMID = '0'
+        tar_gz_path = os.path.join(directory, csv.replace("filelist.csv", "tar.gz"))
+        try:
+            filelist = pd.read_csv(os.path.join(directory, csv))
+            for i in range(len(filelist)):
+                file_path = filelist['Article File'].iloc[i]
+                PMID = str(filelist['PMID'].iloc[i])
+                License = filelist['License'].iloc[i]
+                if not PMID:
+                    PMID = '0'
 
-            PMC_id = file_path[(file_path.find("/PMC") + 1) : -4]
-            if PMC_id in ID_dict.keys():
-                if ID_dict[PMC_id] != PMID:
-                    PMID = ID_dict[PMC_id]
-            
-            if (PMID == '0') or (PMID in PMID_set):
-                continue
-            
-            file_paths.append(directory.split('/')[-1] + '/' + file_path)
-            PMIDs.append(PMID)
-            PMID_set.add(PMID)
-            Licenses.append(License)
+                PMC_id = file_path[(file_path.find("/PMC") + 1) : -4]
+                if PMC_id in ID_dict.keys():
+                    if ID_dict[PMC_id] != PMID:
+                        PMID = ID_dict[PMC_id]
+                
+                if (PMID == '0') or (PMID in PMID_set):
+                    continue
+                
+                tar_gz_paths.append(tar_gz_path)
+                # file_paths.append(directory.split('/')[-1] + '/' + file_path)
+                file_paths.append(file_path)
+                PMIDs.append(PMID)
+                PMID_set.add(PMID)
+                Licenses.append(License)
+        except:
+            print(os.path.join(directory, csv))
+            continue
 
 try:
-    data = pd.DataFrame({"file_path": file_paths, "PMID": PMIDs, "License": Licenses})
+    data = pd.DataFrame({"tar_gz_path": tar_gz_paths, "file_path": file_paths, "PMID": PMIDs, "License": Licenses})
     data.to_csv(os.path.join(data_dir, "PMC_OA_meta.csv"))
 except Exception as e:
     print(e)
--- code/PMC-Patients_collection/PMC_OA_utils/extract_PMC_cites.py
+++ code/PMC-Patients_collection/PMC_OA_utils/extract_PMC_cites.py
@@ -1,9 +1,15 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 import json
 import os
 import xml.etree.cElementTree as ET
 import pandas as pd
-from tqdm import trange
+from tqdm import tqdm,trange
+from glob import glob
+import tarfile
+import shutil
+
+# PMC OA directory
+data_dir = "../../PMC_OA"
 
 """
     Extract referece from PMC OA xml.
@@ -12,35 +18,67 @@ from tqdm import trange
     Output:
         PMID, cites (PMIDs of articles that in this article's reference)
 """
-def extract_cites(msg):
-    file_path, PMID = msg
-    # Some xml files are corrupted.
-    try:
-        tree = ET.parse(os.path.join(data_dir, file_path))
-        root = tree.getroot()
-    except Exception as e:
-        return PMID, []
+# def extract_cites(msg):
+#     file_path, PMID = msg
+#     # Some xml files are corrupted.
+#     try:
+#         # tree = ET.parse(os.path.join(data_dir, file_path))
+#         root = tree.getroot()
+#     except Exception as e:
+#         return PMID, []
+#     cites = []
+#     for ref in root.iterfind(".//ref"):
+#         for id_node in ref.iterfind(".//pub-id"):
+#             # Only consider PMID to track citation.
+#             if id_node is not None and 'pub-id-type' in id_node.attrib and id_node.attrib['pub-id-type'] == "pmid":
+#                 cites.append(id_node.text)
+#     return PMID, cites
+
+
+def getCites(root):
+    if root is None:
+        return []
     cites = []
     for ref in root.iterfind(".//ref"):
         for id_node in ref.iterfind(".//pub-id"):
             # Only consider PMID to track citation.
             if id_node is not None and 'pub-id-type' in id_node.attrib and id_node.attrib['pub-id-type'] == "pmid":
                 cites.append(id_node.text)
-    return PMID, cites
-        
-# PMC OA directory
-data_dir = "../../PMC_OA"
+    return cites
 
-# Results from PMC_OA_meta.py
-file_list = pd.read_csv(os.path.join(data_dir, "PMC_OA_meta.csv"))
+def extract(group):
+    cites = []
+    with tarfile.open(group[0]) as t:
+        for f in tqdm(group[1]['file_path'].values):
+            f_ptr = t.extractfile(f)
+            try:
+                tree = ET.parse(f_ptr)
+                root = tree.getroot()
+            except Exception as e:
+                root = None
+            cites.append(getCites(root))
+    return zip(group[1]['PMID'].map(str).values, cites)
+                  
 
-msgs = [(file_list['file_path'].iloc[i], str(file_list['PMID'].iloc[i])) for i in range(len(file_list))]
-pool = Pool(processes = 15)
-results = pool.map(extract_cites, msgs)
+if __name__ == "__main__":
+    
+    # Required on Windows/macOS when using multiprocessing.spawn
+    try:
+        from multiprocessing import freeze_support
+        freeze_support()
+    except Exception:
+        pass
 
-PMC_cites = {}
-for result in results:
-    if len(result[1]) > 0:
-        PMC_cites[result[0]] = result[1]
+    # Results from PMC_OA_meta.py
+    file_list = pd.read_csv(os.path.join(data_dir, "PMC_OA_meta.csv"))
 
-json.dump(PMC_cites, open(os.path.join(data_dir, "PMC_cites.json"), "w"), indent = 4)
+    data = list(zip(file_list['tar_gz_path'], file_list['file_path'], file_list['PMID']))[:80046]
+ 
+    PMC_cites = {}
+ 
+    groups = [g for g in file_list.groupby("tar_gz_path")]
+    
+    with Pool(5) as pool:
+        PMC_cites.update(dict(pool.map(extract, groups[:50])))
+
+    json.dump(PMC_cites, open(os.path.join(data_dir, "PMC_cites.json"), "w"), indent = 4)
